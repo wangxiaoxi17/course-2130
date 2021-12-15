114c39395537fce230f78ffc432d0f3c9eb9b4e9
wangxiaoxi17 <93524958+wangxiaoxi17@users.noreply.github.com>
2021-12-15 12:03:32 +0300

    Update legacy.py

--- year_2021/python/hw2/legacy.py
+++ year_2021/python/hw2/legacy.py
@@ -1,37 +1,92 @@
+
+from intro import filter_by_indices
+
+
 def t1(number):
-    """
-    Поправьте код, чтобы возвращаемое значение было ближайшим сверху, кратным к 20
-    Пример: number=21 тогда нужно вернуть 40
-    Пример: -5 -> 0
-    """
-    pass
+   
+    now = number // 20
+    now = now * 20
+    if now < number:
+        now += 20
+    return now
 
 
 def t2(string):
-    """
-    На вход подается набор слов, разделенных пробелом, инвертируйте каждое слово.
-    Пример: `abc abc abc` -> `cba cba cba`
-    """
-    pass
+   
+    s = list(string)
+    tot = len(s)
+    print(tot)
+    l = 0
+    while l < tot:
+        while s[l] == ' ':
+            l = l + 1
+        r = l
+        while r < tot and s[r] != ' ':
+            r = r + 1
+        L = l
+        R = r - 1
+        while L < R:
+            s[L], s[R] = s[R], s[L]
+            L = L + 1
+            R = R - 1
+        l = r
+        print(l)
+    return ''.join(s)
 
 
 def t3(dictionary):
-    """
-    На вход подается словарь. Преобразуйте его в строку по следующему шаблону 'key: value; key: value' и так далее
-    """
+    
+    s = ''
+    for key in dictionary:
+        s = s + key.__str__() + ': ' + dictionary[key].__str__() + '; '
+    return s
     pass
 
 
 def t4(string, sub_string):
-    """
-    проверить есть ли в строке инвертированная подстрока
-    """
-    pass
+    
+    sub = list(sub_string)
+    sub.reverse()
+    sub = ''.join(sub)
+    return sub in string
 
 
 def t5(strings):
-    """
-    На вход подается список строк,
-    Отфильтруйте список строк, оставив только строки в формате: `x y z x*y*z`, где x,y,z - целые положительные числа
-    """
+    
+
+    def check(s):
+        tot = len(s)
+        if tot != 8:
+            return False
+        if s[4] != '*':
+            return False
+        if s[6] != '*':
+            return False
+        if s[0] != s[3]:
+            return False
+        if s[1] != s[5]:
+            return False
+        if s[2] != s[7]:
+            return False
+        return s[0].isdigit() and s[1].isdigit() and s[2].isdigit()
+
+    lst = []
+    now = 0
+    for str in strings:
+        ss = str.split(' ')
+        if not check(ss):
+            lst.append(now)
+        now = now + 1
+    filter_by_indices(strings, lst)
     pass
+
+
+m = {1: 2, 3: 3, 4: 5}
+
+print(t1(21))
+print(t2('abc abc abc'))
+print(t3(m))
+print(t4('abc abc abc', 'abc'))
+
+l = []
+l.append('12 13 14 ')
