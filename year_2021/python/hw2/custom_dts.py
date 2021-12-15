a54daa9258b9a7f6c8d308bf7753cd68d67c1495
wangxiaoxi17 <93524958+wangxiaoxi17@users.noreply.github.com>
2021-12-15 11:55:22 +0300

    Update custom_dts.py

--- year_2021/python/hw2/custom_dts.py
+++ year_2021/python/hw2/custom_dts.py
@@ -23,9 +23,16 @@ class CycledList:
     ```
     """
     def __init__(self, size: int):
-        self._data = []
+        self._data = [None]*size
+        self.size = size
+        self.now = 0
+        self.cnt = 0
 
     def append(self, item):
+        self._data[self.cnt] = item
+        self.cnt = (self.cnt + 1) % self.size
+        if self.now != self.size:
+            self.now = self.now + 
         pass
 
 
@@ -43,26 +50,42 @@ class Fraction:
     В каждый момент времени дробь должна быть правильной
 
     """
+    def gcd(self, a, b):
+        if a % b == 0:
+            return b
+        else:
+            return self.gcd(b, a % b)
 
+    
     def __init__(self, nominator, denominator):
-        self.nominator = nominator
-        self.denominator = denominator
+        g = self.gcd(nominator, denominator)
+        self.nominator = nominator / g
+        self.denominator = denominator / g
 
     def __truediv__(self, other):
-        pass
+        return Fraction(self.nominator * other.denominator, self.denominator * other.nominator)
 
     def __add__(self, other):
-        return Fraction(..., ...)
+        a = self.nominator * other.denominator + self.denominator * other.nominator;
+        b = self.denominator * other.denominator
+        return Fraction(a, b)
 
     def __mul__(self, other):
-        pass
+        return Fraction(self.nominator * other.nominator, self.denominator * other.denominator)
 
     def __sub__(self, other: Fraction) -> Fraction:
-        pass
+        a = self.nominator * other.denominator - self.denominator * other.nominator;
+        b = self.denominator * other.denominator
+        return Fraction(a, b)
 
     def __repr__(self):
         return f'{self.nominator}/{self.denominator}'
-
+    
+    class Node:
+    def __init__(self, key):
+        self.key = key
+        self.val = 1
+        self.nxt = None
 
 class MyCounter:
     """
@@ -73,14 +96,59 @@ class MyCounter:
 
     """
 
-    def __init__(self, iterable):
+    
+    def __init__(self, iterable=None):
         self._data = None
+        self.len = 1021
+        self.now = 0
+        self._data = [None] * self.len
+        if iterable is not None:
+            for item in iterable:
+                self.append(item)
+
+    def _hash(self, key):
+        return hash(key) % self.len
 
     def append(self, item):
-        pass
+        pos = self._hash(item)
+        cur = self._data[pos]
+        while cur is not None:
+            if cur.key == item:
+                cur.val = cur.val + 1
+                return
+            cur = cur.nxt
+        node = Node(item)
+        node.nxt = self._data[pos]
+        self._data[pos] = node
 
     def remove(self, item):
-        pass
+        pos = self._hash(item)
+        cur = self._data[pos]
+        if cur is not None:
+            if cur.key == item:
+                cur.val = cur.val - 1
+                if cur.val == 0:
+                    self._data[pos] = cur.nxt
+                    return
+        last = cur
+        cur = cur.nxt
+        while cur is not None:
+            if cur.key == item:
+                cur.val = cur.val - 1
+                if cur.val == 0:
+                    self._data[pos] = cur.nxt
+                return
+            last = cur
+            cur = cur.nxt
+
+    def __getitem__(self, item):  # test
+        pos = self._hash(item)
+        cur = self._data[pos]
+        while cur is not None:
+            if cur.key == item:
+                return cur.val
+            cur = cur.nxt
+        return 0
 
 
 class Figure:
@@ -99,8 +167,19 @@ class Figure:
 
 class Square(Figure):
     """
-    Реализуйте класс квадрат и два метода для него
+   Implement the square class and two methods for it
     """
+
+    def __init__(self, name, len):
+        self.len = len
+        Figure.__init__(self, name)
+
+    def perimeter(self):
+        return self.len
+
+    def square(self):
+        return self.len * self.len
+
     pass
 
 
@@ -126,14 +205,25 @@ class PersistentList:
     Формат файла - json
     """
     def __init__(self, iterable: List[Any], path_to_file: str):
-        pass
+        self.path = path_to_file
+        with open(self.path, 'r', encoding='utf8') as fp:
+            self.list = json.load(fp)
+        if iterable is not None:
+            for item in iterable:
+                self.list.append(item)
+        with open('test.json', 'w', encoding='utf8') as fp:
+            json.dump(self.list, fp, ensure_ascii=False)
 
-    def append(self, item) -> None:
+   def append(self, item) -> None:
         """add item to list"""
-
+        self.list.append(item)
+        with open('test.json', 'w', encoding='utf8') as fp:
+            json.dump(self.list, fp, ensure_ascii=False)
+            
+  
     def __getitem__(self, index):
         """ return item by index """
-        pass
+        return self.list[index]
 
     def delete(self, index: int) -> None:
         """ delete item by index
@@ -144,6 +234,11 @@ class PersistentList:
             if index lower then delete from end of list
 
         """
+        length = len(self.list)
+        index = index % length
+        del self.list[index]
+        with open('test.json', 'w', encoding='utf8') as fp:
+            json.dump(self.list, fp, ensure_ascii=False)
 
     def __repr__(self):
-        pass
+        return 'PersistentList '+self.list.__str__()
