09b10cfeb2383792943896c67fa3196e87ab5439
wangxiaoxi17 <93524958+wangxiaoxi17@users.noreply.github.com>
2021-12-15 12:01:14 +0300

    Update fib.py

--- year_2021/python/hw2/fib.py
+++ year_2021/python/hw2/fib.py
@@ -1,20 +1,24 @@
 
 def fibonacci():
-    """
-    # Задание 4
+   
+    if not hasattr(f, 'x'):
+        f.x = 0
+        return f.x
+    if not hasattr(f, 'y'):
+        f.y = 1
+        return f.y
+    z = f.x + f.y
+    f.x = f.y
+    f.y = z
+    return z
 
-    Написать генератор чисел Фибоначчи
 
-    Input:
-    ```
+def f():
+    if not hasattr(f, 'x'):
+        f.x = 0
+    f.x += 1
+    return f.x
 
-    ```
 
-    Output:
-    ```
-        next call: 0
-        next call: 1
-        ...
-    ```
-    """
-    pass
+for i in range(10):
+    print(fibonacci())
